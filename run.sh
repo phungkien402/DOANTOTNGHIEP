06cceeefc00872ae135a7205258a71bb150f639b
Phung Kien <phungkien4002@gmail.com>
2026-05-21 21:30:15 +0700

    AA

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=/home/phungkien/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-cd /home/phungkien/EHC_HELPDESK/ehc-helpdesk
+cd /home/phungkien/DOANTN
 /usr/bin/python3 "$@"
